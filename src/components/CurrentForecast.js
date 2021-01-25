78719f623b1c32471d7964ab678eab620719db80
haleyelder <haley.elder@live.com>
2021-01-24 19:31:25 -0800

    added extended forecast and formatting

--- src/components/CurrentForecast.js
+++ src/components/CurrentForecast.js
@@ -1,68 +1,57 @@
-import React, {useState, } from 'react'
-import axios from 'axios'
-import LocationData from './LocationData'
-import CurrentConditions from './CurrentConditions'
-import ThreeDayForecast from './ThreeDayForecast'
+import React, { useState } from "react";
+import axios from "axios";
+import LocationData from "./LocationData";
+import CurrentConditions from "./CurrentConditions";
+import ThreeDayForecast from "./ThreeDayForecast";
 
-const REACT_APP_WEATHER_KEY= process.env.REACT_APP_WEATHER_KEY
+const REACT_APP_WEATHER_KEY = process.env.REACT_APP_WEATHER_KEY;
 
 const CurrentForecast = () => {
-  let [locationData, setLocationData] = useState({})
-  let [conditions, setConditions] = useState({})
-  let [city, setCity] = useState('')
-  let [forecast, setForecast] = useState([])
+  let [locationData, setLocationData] = useState({});
+  let [conditions, setConditions] = useState({});
+  let [city, setCity] = useState("");
+  let [forecast, setForecast] = useState([]);
 
-  const getWeather = e => {
+  const getWeather = (e) => {
     e.preventDefault();
-      
-    const getCurrentConditions = () => {
-        return axios.get(` https://api.weatherapi.com/v1/current.json?key=${REACT_APP_WEATHER_KEY}&q=${city}`)
-      }
+    axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${REACT_APP_WEATHER_KEY}&q=${city}&days=3`)
+      .then(function (response) {
+        console.log(response);
 
-      const getForecastConditions = () => {
-        return axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${REACT_APP_WEATHER_KEY}&q=${city}&days=3`)
-      }
+        let locationResponse = response.data.location;
+        let conditionsResponse = response.data.current;
+        let forecastResponse = response.data.forecast.forecastday;
 
-      Promise.all([getCurrentConditions(), getForecastConditions()])
-      .then(function (response) {  
-      
-        let locationResponse = response[0].data.location
-        let conditionsResponse = response[0].data.current
-
-        // first day of three; needs arr loop
-        let forecastResponse = response[1].data.forecast.forecastday[0]
-        
-        console.log(forecastResponse.day.mintemp_f)
-
-        setLocationData(locationResponse)
-        setConditions(conditionsResponse)
-        setForecast(forecastResponse)
+        setLocationData(locationResponse);
+        setConditions(conditionsResponse);
+        setForecast(forecastResponse);
       })
       .catch(function (error) {
-        console.log(error)
-      })     
-  }
+        console.log(error);
+      });
+  };
 
   return (
     <>
-      <div> Current Forecast </div>
-
-      <form onSubmit={getWeather}>
-        <input
-          type="text"
-          placeholder="Search"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-      </form>
-
-      <button type="submit">Get Forecast</button>
+      <div className="current-conditions">
       
-      <LocationData locationData={locationData}/>
-      <CurrentConditions conditions={conditions}/>
-      <ThreeDayForecast forecast={forecast}/>
+        <h3>Current Forecast</h3>
+        <form onSubmit={getWeather}>
+          <input
+            type="text"
+            placeholder="Search"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </form>
+        <button type="submit">Get Forecast</button>
+        <LocationData locationData={locationData} />
+      </div>
+
+      <CurrentConditions conditions={conditions} />
+      <ThreeDayForecast forecast={forecast} />
     </>
-  )
-}
+  );
+};
 
 export default CurrentForecast;
--- src/components/ThreeDayForecast.js
+++ src/components/ThreeDayForecast.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const ThreeDayForecast = (props) => {
-    
-    
-   return (
-        <div>
-            <h3> Three Day Forecast </h3>
-            <p>Date: {props.forecast.date} </p>
-            <p>Min Temp:  {props.forecast.day.mintemp_f}</p>
-            <p>Max Temp: {props.forecast.day.maxtemp_f}&deg;F</p>
-            <p>Chance of Rain: {props.forecast.day.daily_chance_of_rain}%</p>
-            <p>Descript: {props.forecast.day.condition.text}</p>
-            <p>Moon Phase: {props.forecast.astro.moon_phase}</p>
-            <p>Sunrise: {props.forecast.astro.sunrise}</p>
-            <p>Sunset: {props.forecast.astro.sunset}</p>
-        
-            </div>
-    )
-}
+  const forecastList = props.forecast.map((item) => {
+    return (
+      <div key={item}>
+        <p> Date: {item.date}</p>
+        <p> Moon Phase: {item.astro.moon_phase}</p>
+        <p>Min Temp: {item.day.mintemp_f}&deg; F</p>
+        <p>Max Temp: {item.day.maxtemp_f}&deg; F</p>
+        <p>Chance of Rain: {item.day.daily_chance_of_rain}%</p>
+        <p>Descript: {item.day.condition.text}</p>
+        <p>Moon Phase: {item.astro.moon_phase}</p>
+        <p>Sunrise: {item.astro.sunrise}</p>
+        <p>Sunset: {item.astro.sunset}</p>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div className="extended-forecast-block">
+        <h3> Three Day Forecast </h3>
+        {forecastList}
+      </div>
+    </>
+  );
+};
 
 export default ThreeDayForecast;
